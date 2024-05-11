a413d250b4869b7228dee6483413410f07b73931
Thais <thaisvassequi@hotmail.com>
2024-05-11 20:50:50 +0100

    imprimir somente a data, sem a hora

--- src/booking.py
+++ src/booking.py
@@ -10,7 +10,7 @@ def bookingList():
   
 def printBooking(booking):
     print(f"""
-          \033[34mID:\033[0m {booking['id']} \033[34mInício:\033[0m {booking['dataInicio']} \033[34mFim:\033[0m {booking['dataFim']} \033[34mQuantidade de dias:\033[0m {booking['totalDias']} dias
+          \033[34mID:\033[0m {booking['id']} \033[34mInício:\033[0m {booking['dataInicio'].split()[0]} \033[34mFim:\033[0m {booking['dataFim'].split()[0]} \033[34mQuantidade de dias:\033[0m {booking['totalDias']} dias
           \033[34mCliente:\033[0m {booking['cliente_id'][0]} - {booking['cliente_id'][1]}
           \033[34mAutomóvel:\033[0m {booking['automovel_id'][0]} - {booking['automovel_id'][1]}({booking['automovel_id'][2]}) Matrícula:{booking['automovel_id'][4]}
           \033[34mValor total:\033[0m {booking['valorTotal']:.2f}€. Nessa reserva você obteve um desconto de: {booking['desconto']*100:.2f}%.
